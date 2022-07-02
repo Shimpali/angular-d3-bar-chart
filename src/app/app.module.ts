ee8b2cada16570e0dd6b57b6bbcbbd804cd11aae
Shimpali <shimpali.deshpande@yahoo.com>
2022-07-02 12:06:57 +0530

    Minor fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent,  ],
+  declarations: [ AppComponent, BarChartComponent  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
